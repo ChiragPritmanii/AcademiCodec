317406472c271d46babf9711bc595779d74986a9
ChiragPritmanii <cpritmani@gmail.com>
2024-07-11 02:46:25 +0530

    minor changes to path.sh

--- egs/SoundStream_24k_240d/path.sh
+++ egs/SoundStream_24k_240d/path.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export MAIN_ROOT=`realpath ${PWD}/../../`
 
-export PYTHONPATH=${MAIN_ROOT}:${PYTHONPATH}
 MODEL=encodec
-export BIN_DIR=${MAIN_ROOT}/chirag/AcademiCodec/academicodec/models/${MODEL}
+export BIN_DIR=${MAIN_ROOT}/academicodec/models/${MODEL}
